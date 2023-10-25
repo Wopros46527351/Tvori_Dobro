8bd78e12e9bea1029059ee5c96c3a6e81110e269
Wopros46527351 <77889318+Wopros46527351@users.noreply.github.com>
2023-10-25 22:09:33 +0300

    Add files via upload

--- s1.js
+++ s1.js
@@ -15,13 +15,26 @@ if (localStorage.getItem("pageReloaded") !== "true") {
 */
 
 // Ждем 5 секунд
-document.addEventListener("DOMContentLoaded", function() {
-    // Подождем 5 секунд и затем скроем анимацию загрузки и покажем элементы
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Проверяем, авторизован ли пользователь
+    const loggedIn = localStorage.getItem("loggedIn");
+    if (!loggedIn || loggedIn !== "true") {
+    // Если пользователь не авторизован, показываем алерт и перенаправляем его на страницу login.html
+    alert("Пользователь не авторизован.");
+    window.location.href = "vvod.html";
+    }
     setTimeout(function() {
         document.querySelector(".loader").style.display = "none";
         document.querySelector(".content").classList.remove("hidden");
     }, 5000);
-});
+    });
+    
+
+
 function go_on_1() {
     window.location.href = "index.html";
   }
+
+
+  
--- vvod_js.js
+++ vvod_js.js
@@ -1,3 +1,58 @@
 function go_on_1() {
   window.location.href = "index.html";
 }
+
+
+
+
+const signupForm = document.getElementById('signupForm');
+    const loginForm = document.getElementById('loginForm');
+
+    // Обработчик события при отправке формы регистрации
+    signupForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const userName = signupForm.elements.txt.value;
+      const email = signupForm.elements.email.value;
+      const password = signupForm.elements.pswd.value;
+      const storedUserName = localStorage.getItem('email');
+
+      // Проверим, не был ли пользователь ранее зарегистрирован
+      const registeredUsers = JSON.parse(localStorage.getItem("registeredUsers")) || [];
+
+      const userExists = registeredUsers.some(user => user.email === email);
+
+      if (userExists) {
+        alert('Пользователь с таким именем уже существует');
+      } else {
+        // Добавим нового пользователя в список зарегистрированных
+        registeredUsers.push({ userName,email, password });
+        localStorage.setItem("registeredUsers", JSON.stringify(registeredUsers));
+
+        // Сохраните информацию о входе пользователя в localStorage
+        localStorage.setItem("loggedIn", "true");
+        localStorage.setItem("username", userName);
+
+        alert('Регистрация успешно завершена!');
+        signupForm.reset();
+        window.location.href = 'suc_rega.html'; // Перенаправляем на другую страницу
+      }
+    });
+
+    // Обработчик события при отправке формы входа
+    loginForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const registeredUsers = JSON.parse(localStorage.getItem("registeredUsers")) || [];
+      const email = loginForm.elements.email.value;
+      const password = loginForm.elements.pswd.value;
+      
+      const userExists = registeredUsers.some(user => user.email === email && user.password === password);
+
+      if (userExists) {
+        alert('Вход выполнен успешно!');
+        loginForm.reset();
+        window.location.href = 'suc_vhod.html'; // Перенаправляем на другую страницу
+      } else {
+        alert('Неверный email или пароль');
+      }
+    });
